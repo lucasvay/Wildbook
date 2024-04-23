4137cbab82e05a957dfd6872198295713869bcbc
erinz2020 <erinz2020@gmail.com>
2024-04-23 20:27:37 +0000

    refactor BrutalismButton to remove <a> tag and handle navigation programmatically

--- frontend/src/components/home/Progress.jsx
+++ frontend/src/components/home/Progress.jsx
@@ -53,45 +53,7 @@ export default function Progress({
                     <h5>{name}</h5>
                     <span>{encounters} <FormattedMessage id='ENCOUNTERS'/></span>
                 </a>
-            </div>
-            {/* <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-                width: '70%',
-            }}>
-                <div style={{
-                    width: '100%',
-                    height: '15px',
-                    backgroundColor: 'lightgrey',
-                    borderRadius: '3px',
-                    position: 'relative', 
-                    border: '1px solid darkgrey',
-                }}>
-                    <div style={{
-                        position: 'relative',
-                        width: `${progress}%`,
-                        height: '13px',
-                        backgroundColor: 'green',
-                        borderRadius: '3px',
-                    }}>
-                        
-                        <div style={{
-                            position: 'absolute', 
-                            height: '13px',
-                            top: '-3px',
-                            left: progress > 7 ? '30%' : '0',
-                            // transform: progress>10 ? 'translate(-50%, 0)' : 'none',
-                            color: 'white', 
-                            fontSize: '0.8em',
-                            padding: 0,
-                        }}>
-                            {progress}%
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            </div>            
             <ProgressBar progress={progress}/>
         </div>
     );
