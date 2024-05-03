f3776a1c025977e8fa8a5dedbe473387ac1cdaf8
Jon Van Oast <jon@wildme.org>
2024-05-02 19:30:17 -0600

    first pass with uncrustify

--- src/main/java/org/ecocean/grid/ScanWorkItemResult.java
+++ src/main/java/org/ecocean/grid/ScanWorkItemResult.java
@@ -1,8 +1,8 @@
 package org.ecocean.grid;
 
-//unenhanced comment
+// unenhanced comment
 
-//import java.io.Serializable;
+// import java.io.Serializable;
 
 /**
  * An <code>encounter</code> object stores the complete data for a single sighting.
@@ -14,50 +14,45 @@ package org.ecocean.grid;
  * @version 1.2
  */
 public class ScanWorkItemResult implements java.io.Serializable {
-  static final long serialVersionUID = -146404246317385604L;
-  private String uniqueNumWI;
-  private String uniqueNumTask;
-  private String workItemResultUniqueID;
+    static final long serialVersionUID = -146404246317385604L;
+    private String uniqueNumWI;
+    private String uniqueNumTask;
+    private String workItemResultUniqueID;
 
-  private MatchObject result;
-  private I3SMatchObject i3sResult;
-
-  /**
-   * empty constructor required by JDO Enhancer. DO NOT USE.
-   */
-  public ScanWorkItemResult() {
-  }
-
-  public ScanWorkItemResult(String uniqueNumberOfTask, String uniqueNumberOfWorkItem, MatchObject result) {
-    this.result = result;
-    this.uniqueNumWI = uniqueNumberOfWorkItem;
-    this.uniqueNumTask = uniqueNumberOfTask;
-    this.workItemResultUniqueID = uniqueNumberOfWorkItem + "_result";
-    //this.i3sResult = i3sResult;
-  }
-
-  public MatchObject getResult() {
-    return result;
-  }
-
-  public I3SMatchObject getI3SResult() {
-    return i3sResult;
-  }
-
-  /**
-   * Returns the unique number for this workItem.
-   */
-  public String getUniqueNumberWorkItem() {
-    return uniqueNumWI;
-  }
-
-  public String getUniqueNumberWorkItemResult() {
-    return workItemResultUniqueID;
-  }
-
-  public String getUniqueNumberTask() {
-    return uniqueNumTask;
-  }
+    private MatchObject result;
+    private I3SMatchObject i3sResult;
 
 
+    /**
+     * empty constructor required by JDO Enhancer. DO NOT USE.
+     */
+    public ScanWorkItemResult() {}
+
+    public ScanWorkItemResult(String uniqueNumberOfTask, String
+        uniqueNumberOfWorkItem, MatchObject result) {
+        this.result = result;
+        this.uniqueNumWI = uniqueNumberOfWorkItem;
+        this.uniqueNumTask = uniqueNumberOfTask;
+        this.workItemResultUniqueID = uniqueNumberOfWorkItem + "_result";
+        // this.i3sResult = i3sResult;
+    }
+
+    public MatchObject getResult() {
+        return result;
+    }
+    public I3SMatchObject getI3SResult() {
+        return i3sResult;
+    }
+    /**
+     * Returns the unique number for this workItem.
+     */
+    public String getUniqueNumberWorkItem() {
+        return uniqueNumWI;
+    }
+    public String getUniqueNumberWorkItemResult() {
+        return workItemResultUniqueID;
+    }
+    public String getUniqueNumberTask() {
+        return uniqueNumTask;
+    }
 }
